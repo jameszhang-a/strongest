48786fc3c5dda30a72e28dabd7fdac3b2ba7f332
James Zhang <jameszhanga@gmail.com>
2023-02-02 00:13:26 -0800

    enable app dir

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,12 @@ const config = {
   reactStrictMode: true,
   /* If trying out the experimental appDir, comment the i18n config out
    * @see https://github.com/vercel/next.js/issues/41980 */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
+  experimental: {
+    appDir: true,
   },
 };
 export default config;
