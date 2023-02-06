08a85022ef353e9d89c7ddfe4f4fe900dde1fc0f
James Zhang <jameszhanga@gmail.com>
2023-02-05 16:32:58 -0800

    Fix out of bounds error

--- src/utils/getChamps.ts
+++ src/utils/getChamps.ts
@@ -1,8 +1,8 @@
 const NUM_CHAMPS = 162;
 
 export const getOptions = (): [number, number] => {
-  const firstChamp = Math.floor(Math.random() * NUM_CHAMPS) + 1;
-  const secondChamp = Math.floor(Math.random() * NUM_CHAMPS) + 1;
+  const firstChamp = Math.floor(Math.random() * NUM_CHAMPS);
+  const secondChamp = Math.floor(Math.random() * NUM_CHAMPS);
 
   if (firstChamp === secondChamp) {
     return getOptions();
