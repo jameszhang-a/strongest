e8e3374fa3485c1cb114a15d3044e069714c2673
James Zhang <jameszhanga@gmail.com>
2023-02-07 21:25:21 -0800

    Add image placeholder

--- src/components/Champion.tsx
+++ src/components/Champion.tsx
@@ -2,18 +2,35 @@
 import Image from "next/image";
 import React from "react";
 import type { RouterOutputs } from "../utils/api";
+import { blurDataURL } from "../utils/placeholder-img";
 
 type ChampionProp = RouterOutputs["champs"]["getChampByID"];
 
 const Champion: React.FC<{
-  champion: ChampionProp;
-  vote: () => void;
+  champion?: ChampionProp;
+  vote?: () => void;
 }> = ({ champion, vote }) => {
+  // showing a placeholder while loading
+  if (!champion || !vote)
+    return (
+      <div className="w-62 flex aspect-[5/9] h-80 flex-col items-center">
+        <Image
+          src={blurDataURL}
+          alt="placeholder"
+          width="178"
+          height="323"
+        ></Image>
+        <button className="mx20 w-22 btn-warning btn-sm btn mt-4 capitalize">
+          loading...
+        </button>
+      </div>
+    );
+
   const { name, image } = champion;
 
   return (
     <div className="w-62 flex aspect-[5/9] h-80 flex-col items-center">
-      <Image src={image} alt={name} width="320" height="320"></Image>
+      <Image src={image} alt={name} width="178" height="323"></Image>
       <button
         className="mx20 w-22 btn-warning btn-sm btn mt-4 capitalize"
         onClick={() => vote()}
