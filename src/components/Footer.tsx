9160eddb2a2e36daa74278bc41695b4cc701b4cd
James Zhang <jameszhanga@gmail.com>
2023-02-08 03:15:15 -0800

    Minor update to header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
     <div className="flex items-center justify-center bg-[#15162c]">
-      Made by JZ
       <a
         href="https://github.com/jameszhang-a/strongest"
         target="_blank"
         rel="noreferrer"
       >
+        Made by JZ
         <i className="fa-brands fa-github-alt fa-xl relative pl-2"></i>
       </a>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="absolute top-0 flex w-full flex-row justify-center">
+    <div className="sticky top-0 flex w-full flex-row justify-center ">
       <nav className="flex flex-row items-center gap-3">
         <Link
           href="/"
