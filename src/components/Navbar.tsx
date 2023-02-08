effa9481578a33b11739cc77e54bf785cd3b7c68
James Zhang <jameszhanga@gmail.com>
2023-02-08 03:07:06 -0800

    Minor nav bar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,19 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="absolute top-0 flex w-full flex-row justify-center">
-      <nav className="flex flex-row items-center gap-3 ">
-        <Link href="/">Vote</Link>
-        <Link href="/results">Results</Link>
+      <nav className="flex flex-row items-center gap-3">
+        <Link
+          href="/"
+          className="text-[#f2f2f2] hover:text-[#f2f2f2] hover:underline"
+        >
+          Vote
+        </Link>
+        <Link
+          href="/results"
+          className="text-[#f2f2f2] hover:text-[#f2f2f2] hover:underline"
+        >
+          Results
+        </Link>
       </nav>
     </div>
   );
